2b1fccac05ed4493fe463e55c663fdae0a70d6bc
= <avula.usedu1@gmail.com>
2026-03-03 18:41:37 -0600

    demo3 done

--- demo3.js
+++ demo3.js
@@ -1,5 +1,11 @@
 //functional example and adding Events
 
-  
+function clickButton(){
+  alert("Clicked 123456!!!!!!!!!");
+
+}
+const myelement = (
+  <button onClick = {clickButton}>Click the button </button>
+)
   
   ReactDOM.render(myelement, document.getElementById('root'));
